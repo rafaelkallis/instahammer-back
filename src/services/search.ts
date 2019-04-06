8390972846a4023b0da0592d902f7dd8c7fb77e2
Rafael Kallis <rk@rafaelkallis.com>
2019-04-06 17:16:36 +0200

    removed dummy post + cleanup

--- src/services/search.ts
+++ src/services/search.ts
@@ -6,22 +6,20 @@
 import * as algoliasearch from "algoliasearch";
 import { config } from "../config";
 
-
 export class SearchService {
-
   public static async query(query: string): Promise<string[]> {
     const results = await SearchService.index.search({ query });
     return results.hits.map(hit => hit.objectID);
   }
-  
+
   public static async add(id: string, payload: any): Promise<void> {
     await SearchService.index.addObject({ ...payload, objectID: id });
   }
-  
+
   public static async update(id: string, payload: any): Promise<void> {
     await SearchService.index.saveObject({ ...payload, objectID: id });
   }
-  
+
   public static async delete(id: string): Promise<void> {
     await SearchService.index.deleteObject(id);
   }
@@ -30,5 +28,7 @@ export class SearchService {
     config.algoliaApplicationId,
     config.algoliaApiKey
   );
-  private static index = SearchService.client.initIndex(config.algoliaIndexName);
+  private static index = SearchService.client.initIndex(
+    config.algoliaIndexName
+  );
 }
--- src/v1/posts/controller.ts
+++ src/v1/posts/controller.ts
@@ -7,39 +7,6 @@ import { Request, Response } from "express";
 import { RandomService } from "../../services";
 import * as errors from "../errors";
 
-function DummyPost() {
-  return {
-    id: "some-id",
-    title: "Pump Failure",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et hendrerit mauris. Integer consectetur tempor augue quis pulvinar. Nullam non vestibulum tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce lacinia sem felis, non pretium sem cursus sed. Nunc et faucibus odio. Donec vulputate pellentesque nulla vel consectetur. Sed blandit hendrerit quam nec suscipit.",
-    createdAt: 1554550564,
-    location: {
-      latitude: 47.5056,
-      longitude: 8.7241
-    },
-    postTags: ["pump", "A3000", "corosion"],
-    image:
-      "https://www.bouldencompany.com/images/site/Vespel%20Images/Vespel%20Case%20Studies%20LPG%20Pump%201.jpg",
-    imageTags: [
-      {
-        text: "corosion",
-        pos: {
-          x: 50,
-          y: 30
-        }
-      },
-      {
-        text: "cracks",
-        pos: {
-          x: 40,
-          y: 70
-        }
-      }
-    ]
-  };
-}
-
 export class PostController {
   /**
    * Handler for adding a post.
